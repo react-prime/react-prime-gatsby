3ebfa6b6049ae440f09342bf8870575abb3c0e0e
sandervspl <sandervispoel@hotmail.com>
2021-08-16 11:23:25 +0200

    Update Gatsby packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,98 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "react-prime-gatsby",
+    siteUrl: process.env.GATSBY_SITE_URL,
+    title: 'react-prime-gatsby',
+    titleTemplate: '%s - Nowatch Website',
   },
   plugins: [
+    // {
+    //   resolve: 'gatsby-source-contentful',
+    //   options: {
+    //     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    //     spaceId: process.env.CONTENTFUL_SPACE_ID,
+    //     host: process.env.CONTENTFUL_HOST,
+    //     environment: process.env.CONTENTFUL_ENVIRONMENT,
+    //   },
+    // },
+    // {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     trackingId: process.env.GA_TRACKING_ID,
+    //   },
+    // },
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        accessToken: "",
-        spaceId: "",
+        name: 'react-prime-gatsby',
+        short_name: 'gatsby',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#000',
+        display: 'standalone',
+        icon: 'src/assets/images/icon.png',
       },
     },
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        trackingId: "",
+        name: 'images',
+        path: './src/assets/images/',
+      },
+      __key: 'images',
+    },
+    // {
+    //   resolve: 'gatsby-plugin-typegen',
+    //   options: {
+    //     outputPath: 'src/__generated__/gatsby-types.d.ts',
+    //     emitSchema: {
+    //       'src/__generated__/gatsby-schema.graphql': true,
+    //       'src/__generated__/gatsby-introspection.json': true,
+    //     },
+    //     emitPluginDocuments: {
+    //       'src/__generated__/gatsby-plugin-documents.graphql': true,
+    //     },
+    //   },
+    // },
+    {
+      resolve: 'gatsby-plugin-module-resolver',
+      options: {
+        root: './src',
+        aliases: {
+          components: './components',
+          common: './components/common',
+          modules: './components/modules',
+          pages: './pages',
+          styles: './styles',
+          services: './services',
+          ducks: './ducks',
+          selectors: './selectors',
+          templates: './templates',
+          images: './assets/images',
+          fonts: './assets/fonts',
+          videos: './assets/videos',
+          vectors: './assets/vectors',
+          css: './assets/css',
+          types: './types',
+          static: {
+            root: './public',
+            alias: './static',
+          },
+        },
       },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-react-svg',
       options: {
-        icon: "src/images/icon.png",
+        rule: {
+          include: `${__dirname}/src/assets/vectors/`,
+        },
       },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
     },
   ],
 };
